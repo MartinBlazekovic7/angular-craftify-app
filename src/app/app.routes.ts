635b6d23c0ff2cc788b896d512c1c1a9490361aa
mblazekovic <martinblazekovic7@gmail.com>
2024-05-15 01:53:56 +0200

    CRAFT-83 - Create navigation menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,72 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'news',
+    loadComponent: () =>
+      import('./pages/news/news.component').then((m) => m.NewsComponent),
+  },
+  {
+    path: 'tutorials',
+    loadComponent: () =>
+      import('./pages/tutorials/tutorials.component').then(
+        (m) => m.TutorialsComponent
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'registration',
+    loadComponent: () =>
+      import('./pages/registration/registration.component').then(
+        (m) => m.RegistrationComponent
+      ),
+  },
+  {
+    path: 'favorites',
+    loadComponent: () =>
+      import('./pages/favorites/favorites.component').then(
+        (m) => m.FavoritesComponent
+      ),
+  },
+  {
+    path: 'settings',
+    loadComponent: () =>
+      import('./pages/settings/settings.component').then(
+        (m) => m.SettingsComponent
+      ),
+  },
+  {
+    path: 'admin-dashboard',
+    loadComponent: () =>
+      import('./pages/admin-dashboard/admin-dashboard.component').then(
+        (m) => m.AdminDashboardComponent
+      ),
+  },
+  {
+    path: 'profile/:id',
+    loadComponent: () =>
+      import('./pages/profile/profile.component').then(
+        (m) => m.ProfileComponent
+      ),
+  },
+  {
+    path: 'project/:slug',
+    loadComponent: () =>
+      import('./pages/project/project.component').then(
+        (m) => m.ProjectComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SidebarModule,
+    ButtonModule,
+    OverlayPanelModule,
+  ],
   templateUrl: './navigation-bar.component.html',
-  styleUrl: './navigation-bar.component.scss'
+  styleUrl: './navigation-bar.component.scss',
 })
 export class NavigationBarComponent {
-
+  sidebarVisible = false;
 }
