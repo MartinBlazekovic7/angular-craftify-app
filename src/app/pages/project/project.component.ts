50e5f8e2daaedbf1aa884747c557590c06713efc
mblazekovic <martinblazekovic7@gmail.com>
2024-05-22 00:26:25 +0200

    CRAFT-94 - Create project details page

--- src/app/pages/project/project.component.ts
+++ src/app/pages/project/project.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProjectService } from '../../services/project.service';
+import { Project } from '../../models/project.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommentsComponent } from '../../components/comments/comments.component';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [CommentsComponent],
   templateUrl: './project.component.html',
-  styleUrl: './project.component.scss'
+  styleUrl: './project.component.scss',
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit, OnDestroy {
+  subscription?: Subscription;
 
+  project?: Project;
+
+  projectId?: number;
+
+  inFavorites = false;
+
+  constructor(
+    private projectService: ProjectService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.getProjectId();
+    this.getProject();
+  }
+
+  getProjectId(): void {
+    this.subscription = this.route.params.subscribe((params) => {
+      this.projectId = params['id'];
+    });
+  }
+
+  getProject(): void {
+    if (!this.projectId) {
+      return;
+    }
+
+    this.subscription = this.projectService
+      .getProjectById(this.projectId)
+      .subscribe({
+        next: (project: Project) => {
+          this.project = project;
+        },
+        error: () => {
+          this.router.navigate(['/']);
+        },
+      });
+  }
+
+  likeProject(): void {}
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
