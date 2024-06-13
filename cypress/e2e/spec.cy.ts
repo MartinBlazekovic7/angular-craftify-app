ffda7650079c392eaec3cf3d05a2a66774981797
mblazekovic <martinblazekovic7@gmail.com>
2024-06-13 18:15:22 +0200

    Cypress testing

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -104,3 +104,79 @@ describe('Registracija novog korisnika', () => {
     cy.get('.successModal > .ng-star-inserted').click();
   });
 });
+
+describe('Prijava', () => {
+  it('Korisnik posjeti stranicu', () => {
+    cy.url().should('include', 'http://localhost:4200/login');
+  });
+  it('Korisnik unosi podatke', () => {
+    cy.get('[type="text"]').type('john@example.com');
+    cy.get('[type="password"]').type('newPassword123');
+  });
+  it('Korisnik pritišće SUBMIT gumb', () => {
+    cy.get('form.ng-dirty > button').click({ force: true });
+    cy.get('.successModal > .ng-star-inserted').click();
+  });
+  it('Provjeriti da li je korisnik prebačen na HOME stranicu', () => {
+    cy.url().should('include', 'http://localhost:4200/');
+  });
+  it('Provjeriti da li se učitala navigacijska traka', () => {
+    cy.get('.navigation__Bar').should('be.visible');
+  });
+  it('Provjeriti da li se učitao meni', () => {
+    cy.get('.ng-star-inserted > .pi').click();
+    cy.get('[ng-reflect-router-link="profile"]').should('be.visible');
+  });
+});
+
+describe('Korisnički profil', () => {
+  it('Korisnik ulazi u svoj profil', () => {
+    cy.get('[ng-reflect-router-link="profile"]').click();
+  });
+  it('Provjera je li korisnik na svom profilu', () => {
+    cy.url().should('include', 'http://localhost:4200/profile');
+  });
+  it('Provjera elemenata', () => {
+    cy.get('h1').should('be.visible');
+    cy.get('.userProfile__Header > :nth-child(2)').should('be.visible');
+    cy.get('.userProfile__Header > :nth-child(3)').should('be.visible');
+    cy.get('.userProfile__Projects').should('be.visible');
+    cy.get('.userProfile__Comments').should('be.visible');
+    cy.get('.userProfile__Likes').should('be.visible');
+  });
+  it('Provjera korisničkog imena', () => {
+    cy.get('.userProfile__Header > :nth-child(3)').should(
+      'have.text',
+      'Username: john_doe'
+    );
+  });
+});
+
+describe('Kreiranje projekta', () => {
+  it('Korisnik ulazi u svoj profil', () => {
+    cy.get('.ng-star-inserted > .pi').click();
+    cy.get('[ng-reflect-router-link="profile"]').click();
+  });
+  it('Odlazak na formu za kreiranje projekta', () => {
+    cy.get('.userProfile__Projects > .main-button').click();
+  });
+  it('Upisivanje podataka u formu', () => {
+    cy.get('#title').type('Test project');
+    cy.get('#content').type(
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    );
+    cy.get('#description').type('Test project decription');
+    cy.get('#category').select('Home Decor');
+    cy.get('#complexity').select('Beginner');
+    cy.get('form.ng-dirty > button').click();
+  });
+});
+describe('Provjera projekta', () => {
+  it('Korisnik ulazi u svoj profil', () => {
+    cy.get('.ng-star-inserted > .pi').click();
+    cy.get('[ng-reflect-router-link="profile"]').click();
+  });
+  it('Provjera novog projekta', () => {
+    cy.get(':nth-child(2) > h3').should('have.text', 'Test project');
+  });
+});
